54334b7dac5af4304e01378eea367b9ce76d2922
Aflah Tazakka <aflahtazakka@yahoo.com>
2023-11-07 22:13:02 +0700

    Exercise 3.12. completed

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const input_name = process.argv[3]
+const input_number = process.argv[4]
+
+const url =
+  `mongodb+srv://aflaht:${password}@fsocluster0.resupuh.mongodb.net/personApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: input_name,
+  number: input_number,
+})
+
+person.save().then(result => {
+  console.log('phonebook:')
+  Person
+  .find({})
+  .then(persons => {
+    persons.map(p => console.log(`${p.name} ${p.number}`))
+    mongoose.connection.close()
+  })
+})
